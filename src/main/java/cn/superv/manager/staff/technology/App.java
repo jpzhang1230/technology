6246dfb5f01a8d31b8b7f8ca00041e6ee93895c1
jpzhang <jpzhang@superv.com.cn>
2013-08-18 12:47:12 +0800

    项目框架

---
+++ src/main/java/cn/superv/manager/staff/technology/App.java
@@ -0,0 +1,13 @@
+package cn.superv.manager.staff.technology;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
